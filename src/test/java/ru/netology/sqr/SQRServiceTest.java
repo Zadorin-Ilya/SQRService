21bf0fcb33d903434e3a7184519433679156173e
Илья Задорин <zadorinilja@yandex.ru>
2022-08-12 13:35:40 +0400

    SQRService

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.sqr;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+
+    @Test
+    public void test () {
+        SQRService service = new SQRService();
+        int actual = service.calculate(200, 300);
+
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testzero () {
+        SQRService service = new SQRService();
+        int actual = service.calculate(0, 0);
+
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
+
+

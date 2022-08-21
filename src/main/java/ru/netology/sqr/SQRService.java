b003a86ac858d8c854c88bea2847e550a8d9ca35
Илья Задорин <zadorinilja@yandex.ru>
2022-08-21 12:52:08 +0400

    SQRService2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,14 +4,11 @@ package ru.netology.sqr;
 public class SQRService {
     public int calculate(int min, int max) {
         int count = 0;
+        int ii;
         for (int i = 10; i <= 99; i++) {
-            int ii = i * i;
+            ii = i * i;
             if (ii >= min) {
-                if (ii <= max) ;
-                {
-                    count++;
-                }
-
+                count++;
             }
         }
         return count;
